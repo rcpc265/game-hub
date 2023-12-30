4620c94e277c55e1f842a9c2d59725293a36b663
Rafael Cangahuala <78826890+rcpc265@users.noreply.github.com>
2023-12-30 11:43:02 -0500

    Display critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,16 @@
+import { Badge } from "@chakra-ui/react";
+
+interface CriticScoreProps {
+  score: number;
+}
+
+const CriticScore = ({ score }: CriticScoreProps) => {
+  const color = score > 75 ? "green" : score > 50 ? "yellow" : "red";
+
+  return (
+    <Badge colorScheme={color} fontSize="14px" paddingX={2} borderRadius="4px">
+      {score}
+    </Badge>
+  );
+};
+export default CriticScore;
